628eb8ed52e674c8082123b94979be92982e380d
Fortivera <ant97str@gmail.com>
2022-08-21 14:45:46 -0400

    Adjust image scaling

--- recipes/settings.py
+++ recipes/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# Make sure to set the value to True when in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
